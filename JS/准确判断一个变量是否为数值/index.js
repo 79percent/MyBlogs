7373f8b218ab8eabafb75637f62ccf5024d25dd0
per <per7614@163.com>
2020-12-23 17:20:28 +0800

    12.23

---
+++ JS/准确判断一个变量是否为数值/index.js
@@ -0,0 +1,55 @@
+/**
+ * 准确判断一个变量是否为数字
+ * @param {*} n 
+ */
+const verifyNum = (n) => {
+  switch (n) {
+    case null:
+    case undefined:
+      console.error('argument is not a number')
+      return;
+    default:
+      const type_n = typeof n;
+      switch (type_n) {
+        case 'number':
+        case 'string':
+        case 'number':
+          const num = Number(n);
+          if (Number.isNaN(num)) {
+            console.error('argument is not a number')
+            return;
+          }
+          console.log(num)
+          return num;
+        case 'object':
+          if (n instanceof Number) {
+            console.log(Number(n))
+            return Number(n);
+          }
+          console.error('argument is not a number')
+          return;
+        default:
+          console.error('argument is not a number')
+          return;
+      }
+  }
+}
+
+verifyNum(0)
+verifyNum(-0)
+verifyNum(+0)
+verifyNum(1)
+verifyNum('2')
+verifyNum(Number(3))
+verifyNum(true)
+verifyNum(false)
+verifyNum(null)
+verifyNum(undefined)
+verifyNum(NaN)
+verifyNum({})
+verifyNum({ x: 1 })
+verifyNum({ valueOf: function () { return 2 } })
+verifyNum([])
+verifyNum([1, 2, 3])
+verifyNum(function () { })
+verifyNum(Symbol(1))
