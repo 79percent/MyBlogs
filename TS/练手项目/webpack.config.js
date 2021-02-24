ce2b0d2db4b291dec028df36c38a768838db3632
per <per7614@163.com>
2021-02-24 18:02:15 +0800

    2.24

---
+++ TS/练手项目/webpack.config.js
@@ -0,0 +1,82 @@
+const path = require('path');
+const { Configuration } = require('webpack');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+
+/**
+ * @type {Configuration}
+ */
+const config = {
+  entry: './src/index.ts',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'bundle.js',
+    environment: {
+      arrowFunction: false
+    }
+  },
+  module: {
+    rules: [
+      {
+        test: /\.ts$/,
+        use: [
+          {
+            loader: 'babel-loader',
+            options: {
+              presets: [
+                [
+                  "@babel/preset-env",
+                  {
+                    targets: {
+                      "chrome": '58',
+                      "ie": '11'
+                    },
+                    "corejs": '3',
+                    "useBuiltIns": 'usage'
+                  }
+                ]
+              ]
+            }
+          },
+          'ts-loader'
+        ],
+        exclude: path.resolve(__dirname, 'node_modules')
+      },
+      /** 编译less文件 */
+      {
+        test: /\.less$/,
+        use: [
+          "style-loader",
+          "css-loader",
+          {
+            loader: "postcss-loader",
+            options: {
+              postcssOptions: {
+                plugins: [
+                  [
+                    'postcss-preset-env',
+                    {
+                      browsers: 'last 2 versions',
+                    }
+                  ]
+                ]
+              }
+            }
+          },
+          "less-loader",
+        ]
+      }
+    ]
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './src/index.html'
+    }),
+    new CleanWebpackPlugin(),
+  ],
+  resolve: {
+    extensions: ['.ts', '.js']
+  }
+}
+
+module.exports = config
